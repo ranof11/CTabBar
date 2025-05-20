8c8b21cc75090076614fbd5f1222f68bad809906
ranof11 <rajeshtriadi14@gmail.com>
2025-05-20 13:26:22 +0700

    Initial Commit

---
+++ CTabBar/CTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTabBarApp.swift
+//  CTabBar
+//
+//  Created by Rajesh Triadi Noftarizal on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
